72915dc3b4b055aec4493f3a441c4abe21aff7c8
AkshayShewate <aksshewate779@gmail.com>
2020-08-19 15:56:25 +0530

    Alan Voice Ai News App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import alanBtn from '@alan-ai/alan-sdk-web'
+import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles.js';
+import wordsToNumbers  from 'words-to-numbers';
 
-function App() {
+ const alanKey = 'ff3593f5e82ca55a4baa333ae12b45722e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App=() => {
+
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
+  const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles, number }) =>{
+        if(command === 'newHeadlines'){ 
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        }else if(command === 'highlight'){ 
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        }else if(command === 'open'){
+
+          const parsedNumber = number.length > 2 ? wordsToNumbers(number, { fuzzy: true}) : number;
+          const article = articles[parsedNumber - 1];
+          if(parsedNumber > 20) {
+            alanBtn().playText('Please try that again.')
+          } else if(article) {
+            window.open(articles.url, '_blank');
+            alanBtn().playText('Opening...');
+          }          
+        }
+      }
+    })
+    }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={classes.logoContainer}>
+        <img src="https://image.shutterstock.com/image-vector/chatbot-icon-bot-sign-design-260nw-740191156.jpg" className={classes.alanLogo} alt="alan logo"/>
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
     </div>
   );
 }
